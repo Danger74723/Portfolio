485ab7b44387776f4b4b004eca5b3659df4e94c8
Danger74723 <bipadtaranmahato9@gmail.com>
2026-01-20 13:01:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed= new Typed(".text1",{
+    strings:["Frontend Developer","Web Developer","Grapic Designer"],
+    typespeed:100,
+    backspeed:100,
+    backdelay:1000,
+    loop:true
+});
